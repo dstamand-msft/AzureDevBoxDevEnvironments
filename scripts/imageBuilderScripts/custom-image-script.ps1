e3f0e5c47c0c5be51f8e320df9ad0c29d7892019
Ayoub KEBAILI <keayoub@msn.com>
2023-08-16 23:16:29 -0400

    update script

--- scripts/imageBuilderScripts/custom-image-script.ps1
+++ scripts/imageBuilderScripts/custom-image-script.ps1
@@ -1,4 +1,9 @@
+
+Write-Host "Installing Chocolatey..."
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Write-Host "Chocolatey Installed"
+
+Write-Host "Installing Packages..."
 choco install -y vscode
 choco install 7zip -y
 choco install adobereader -y
@@ -8,11 +13,16 @@ choco install kubernetes-cli -y
 choco install kubernetes-helm -y
 choco install pwsh -y
 choco install terraform -y
-choco install dotnet
+Write-Host "Packages Installed"
 
+Write-Host "Installing Azure CLI..."
 # Clone Application from azure
-git clone 'https://github.com/dockersamples/example-voting-app.git' voting-app
 
+# Create-folder
+New-Item -Path "C:\workingdir" -ItemType "directory" -Force
+Set-Location "C:\workingdir"
+git clone 'https://github.com/dockersamples/example-voting-app.git' voting-app
+Write-Host "Cloning Application from Azure"
 # Run application
 $cwd = (Get-Location)
 Set-Location $cwd/voting-app
