397ba030e0d47c66d0ef38fedf9535ca4927e472
Ayoub KEBAILI <keayoub@msn.com>
2023-08-18 11:32:44 -0400

    update script

--- scripts/imageBuilderScripts/CreateImageBuilderIdentity.ps1
+++ scripts/imageBuilderScripts/CreateImageBuilderIdentity.ps1
@@ -1,4 +1,9 @@
 Write-Host ""
+Write-Host "Installing required Az modules..."
+Write-Host ""
+'Az.Resources', 'Az.ImageBuilder', 'Az.ManagedServiceIdentity', 'Az.Compute' | ForEach-Object { Install-Module -Name $_ -AllowPrerelease -AllowClobber -Confirm:$true -AcceptLicense }
+
+Write-Host ""
 Write-Host "Loading azd .env file from current environment"
 Write-Host ""
 
@@ -12,9 +17,9 @@ foreach ($line in $output) {
     $value = $value -replace '^\"|\"$'
     [Environment]::SetEnvironmentVariable($name, $value)
 }
+Write-Host ""
 Write-Host "Environment variables set."
-
-'Az.Resources', 'Az.ImageBuilder', 'Az.ManagedServiceIdentity', 'Az.Compute' | ForEach-Object { Install-Module -Name $_ -AllowPrerelease -AllowClobber -Confirm:$false }
+Write-Host ""
  
 # Get your current subscription ID  
 $subscriptionID = "$env:AZURE_SUBSCRIPTION_ID"
@@ -28,23 +33,22 @@ $identityName = "$env:AZURE_IMAGE_BUILDER_IDENTITY"
 $imageRoleDefName = "Azure Image Builder Image Def " + $identityName 
   
 #Check if the identity already exists
+Write-Host ""
 Write-Host "Check if the identity already exists..."
+Write-Host ""
+
 $identity = Get-AzUserAssignedIdentity -SubscriptionId $subscriptionID -ResourceGroupName $imageResourceGroup -Name $identityName -ErrorAction SilentlyContinue
-#$identityNameResourceId = $null
 $identityNamePrincipalId = $null
 
 if ($null -ne $identity) {
     Write-Information "Identity already exists, skipping creation"
-    #$identityNameResourceId = $identity.Id 
     $identityNamePrincipalId = $identity.PrincipalId
 }
 else {
     # Create an identity 
     New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName -Location $location
-    #$identityNameResourceId = $(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).Id 
     $identityNamePrincipalId = $(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).PrincipalId
 
-    
     Write-Host "Check if role definition already exists..."
     # check if role definition already exists
     $roleDef = Get-AzRoleDefinition -Name $imageRoleDefName -ErrorAction SilentlyContinue
@@ -69,4 +73,3 @@ else {
         New-AzRoleAssignment -ObjectId $identityNamePrincipalId -RoleDefinitionName $imageRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
     }
 }
-
