c39f3fcb17f32a06159c29cfd9cf2c8804988364
Ayoub KEBAILI <keayoub@msn.com>
2023-08-21 17:03:30 -0400

    update scripts

--- scripts/imageBuilderScripts/choco-vscode.ps1
+++ scripts/imageBuilderScripts/choco-vscode.ps1
@@ -1,2 +1,12 @@
+### Start  Script 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install -y vscode
+choco install vscode -y
+choco install -y azure-cli
+choco install pwsh -y
+choco install azd -y
+choco install git -y
+choco install python -y
+choco install nodejs -y
+# Refresh path variables
+RefreshEnv.cmd
+### End
